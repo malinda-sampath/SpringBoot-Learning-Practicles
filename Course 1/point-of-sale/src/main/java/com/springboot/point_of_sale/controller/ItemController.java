6131479163ce2b133bf28237ae025931e8e9d954
malinda-sampath <malindasampath45@gmail.com>
2026-02-03 01:04:26 +0530

    Standard Response

--- Course 1/point-of-sale/src/main/java/com/springboot/point_of_sale/controller/ItemController.java
+++ Course 1/point-of-sale/src/main/java/com/springboot/point_of_sale/controller/ItemController.java
@@ -3,6 +3,9 @@ package com.springboot.point_of_sale.controller;
 import com.springboot.point_of_sale.dto.request.ItemRequestDTO;
 import com.springboot.point_of_sale.dto.response.ItemResponseDTO;
 import com.springboot.point_of_sale.service.ItemService;
+import com.springboot.point_of_sale.util.response.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,17 +22,35 @@ public class ItemController {
     }
 
     @PostMapping(path = "/save")
-    public String itemSave(@RequestBody ItemRequestDTO itemRequestDTO){
-        return itemService.save(itemRequestDTO);
+    public ResponseEntity<StandardResponse<String>> itemSave(@RequestBody ItemRequestDTO itemRequestDTO){
+        itemService.save(itemRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new StandardResponse<>(
+                        201,
+                        "Item created successfully",
+                        null
+                ));
     }
 
     @GetMapping(path = "/get-by-name",params = "name")
-    public List<ItemResponseDTO> getByName(@RequestParam(value = "name") String itemName){
-        return itemService.getByNameByMapstruct(itemName);
+    public ResponseEntity<StandardResponse<List<ItemResponseDTO>>> getByName(@RequestParam(value = "name") String itemName){
+        List<ItemResponseDTO> data = itemService.getByNameByMapstruct(itemName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new StandardResponse<>(
+                        200,
+                        "Get items by name",
+                        data
+                ));
     }
 
     @GetMapping(path = "/get-all-items")
-    public List<ItemResponseDTO> getAllItems(){
-        return itemService.getAllItems();
+    public ResponseEntity<StandardResponse<List<ItemResponseDTO>>> getAllItems(){
+        List<ItemResponseDTO> data = itemService.getAllItems();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new StandardResponse<>(
+                        200,
+                        "Get all customers",
+                        data
+                ));
     }
 }
