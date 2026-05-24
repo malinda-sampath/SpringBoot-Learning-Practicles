4db3fd40805c30f063d4c63ca8474343fc9d22ea
malinda-sampath <malindasampath45@gmail.com>
2026-05-25 00:26:10 +0530

    microservice communication - resttemplate

--- Course 3/2_micro-service - communication/1_resetTemplate/employee-service/src/main/java/com/microservice/employee_service/service/impl/EmployeeServiceIMPL.java
+++ Course 3/2_micro-service - communication/1_resetTemplate/employee-service/src/main/java/com/microservice/employee_service/service/impl/EmployeeServiceIMPL.java
@@ -1,11 +1,18 @@
 package com.microservice.employee_service.service.impl;
 
+import com.microservice.employee_service.dto.ApiResponseDTO;
+import com.microservice.employee_service.dto.DepartmentDTO;
 import com.microservice.employee_service.dto.EmployeeDTO;
 import com.microservice.employee_service.entity.Employee;
 import com.microservice.employee_service.repository.EmployeeRepo;
 import com.microservice.employee_service.service.EmployeeService;
 import com.microservice.employee_service.utill.mappers.EmployeeMapper;
+import com.microservice.employee_service.utill.response.StandardResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,10 +21,12 @@ public class EmployeeServiceIMPL implements EmployeeService {
 
     private final EmployeeRepo employeeRepo;
     private final EmployeeMapper employeeMapper;
+    private final RestTemplate restTemplate;
 
-    public EmployeeServiceIMPL(EmployeeRepo employeeRepo, EmployeeMapper employeeMapper) {
+    public EmployeeServiceIMPL(EmployeeRepo employeeRepo, EmployeeMapper employeeMapper, RestTemplate restTemplate) {
         this.employeeRepo = employeeRepo;
         this.employeeMapper = employeeMapper;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -33,8 +42,19 @@ public class EmployeeServiceIMPL implements EmployeeService {
     }
 
     @Override
-    public EmployeeDTO getEmployeeById(int id) {
+    public ApiResponseDTO getEmployeeById(int id) {
         Employee employee = employeeRepo.getReferenceById((long) id);
-        return employeeMapper.entityToDto(employee);
+
+        ResponseEntity<StandardResponse<DepartmentDTO>> responseEntity = restTemplate.exchange(
+                "http://localhost:8082/api/department/get-by-code?code=" + employee.getDepartmentCode(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+        });
+
+        assert responseEntity.getBody() != null;
+        DepartmentDTO departmentDTO = responseEntity.getBody().getData();
+
+        return new ApiResponseDTO(employeeMapper.entityToDto(employee), departmentDTO);
     }
 }
