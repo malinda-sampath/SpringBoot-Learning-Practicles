1bc3e34009f8182db2975557082fe3b926317fe0
malinda-sampath <malindasampath45@gmail.com>
2026-06-04 01:07:04 +0530

    api gateway - initial commit

---
+++ Course 3/4_api-gateway/department-service/src/main/java/com/microservice/department_service/utill/response/ResponseBuilder.java
@@ -0,0 +1,34 @@
+package com.microservice.department_service.utill.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseBuilder {
+
+    private ResponseBuilder(){}
+
+    // 200
+    public static <T> ResponseEntity<StandardResponse<T>> ok(String message, T data) {
+        return build(HttpStatus.OK, message, data);
+    }
+
+    // 201
+    public static <T> ResponseEntity<StandardResponse<T>> created(String message, T data) {
+        return build(HttpStatus.CREATED, message, data);
+    }
+
+    // ===== CORE BUILDER =====
+
+    private static <T> ResponseEntity<StandardResponse<T>> build(
+            HttpStatus status,
+            String message,
+            T data
+    ) {
+        return ResponseEntity.status(status)
+                .body(new StandardResponse<>(
+                        status.value(),
+                        message,
+                        data
+                ));
+    }
+}
---
+++ Course 3/4_api-gateway/employee-service/src/main/java/com/microservice/employee_service/dto/DepartmentDTO.java
@@ -0,0 +1,16 @@
+package com.microservice.employee_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDTO {
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+}
