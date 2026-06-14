9d7cbb0ae5b545408038c571a7dbc20acaf51077
malinda-sampath <malindasampath45@gmail.com>
2026-06-15 02:58:48 +0530

    Inmemory service manager

--- Course 2/point-of-sale/src/main/java/com/springboot/point_of_sale/config/ProjectSecurityConfig.java
+++ Course 2/point-of-sale/src/main/java/com/springboot/point_of_sale/config/ProjectSecurityConfig.java
@@ -3,7 +3,14 @@ package com.springboot.point_of_sale.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -21,5 +28,16 @@ public class ProjectSecurityConfig {
 
         return http.build();
 
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
     }
 }
