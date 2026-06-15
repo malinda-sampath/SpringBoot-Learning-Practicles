b18cb3b9c296220a4ab1aabbb087ae1b5f078df6
malinda-sampath <malindasampath45@gmail.com>
2026-06-15 23:20:54 +0530

    Enable method security

--- Course 2/point-of-sale/src/main/java/com/springboot/point_of_sale/config/ProjectSecurityConfig.java
+++ Course 2/point-of-sale/src/main/java/com/springboot/point_of_sale/config/ProjectSecurityConfig.java
@@ -2,11 +2,13 @@ package com.springboot.point_of_sale.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class ProjectSecurityConfig {
 
     @Bean
@@ -14,6 +16,7 @@ public class ProjectSecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/v1/notice/my-notices","api/v1/user/register").permitAll()
+//                        .requestMatchers("/api/v1/account/my-account").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
